0947f55536cf1b89b8fb6ecdf2aa2319af85aa55
MD. Zihad Islam <159674523+Zihadvie@users.noreply.github.com>
2025-09-27 13:30:59 +0600

    Create Sum_of_Digit.c
    sum of digit add how to this can learn

---
+++ Sum_of_Digit.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+	// your code goes here
+   int a,b,c=0;
+  scanf("%d",&a);
+  while(a>0){
+   b=a%10;
+ // printf("%d",b);
+ 
+  c=c+b;
+   a=a/10;
+
+    
+  }
+     printf("%d",c);
+
+}
